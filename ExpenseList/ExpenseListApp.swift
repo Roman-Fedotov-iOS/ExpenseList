5d453907d866b8bc6590077e0335be47a191f29b
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-25 23:12:03 +0300

    Initial Commit

---
+++ ExpenseList/ExpenseListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseListApp.swift
+//  ExpenseList
+//
+//  Created by Roman Fedotov on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
