68a1fbe424a9a8972d645964cb717d72db817523
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-26 14:21:10 +0300

    1

--- ExpenseList/ContentView.swift
+++ ExpenseList/ContentView.swift
@@ -7,10 +7,68 @@
 
 import SwiftUI
 
+struct ExpensesItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpensesItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items
+            ) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpensesItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var showAdd = false
+    @ObservedObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("Expenses")
+            .navigationBarItems(trailing: Button (action: {
+                self.showAdd = true
+            }) {
+                Image(systemName: "plus")
+            }
+            )
+            .sheet(isPresented: $showAdd) {
+                AddView(expenses: self.expenses)
+            }
+        }
+    }
+    func removeItems(as offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
